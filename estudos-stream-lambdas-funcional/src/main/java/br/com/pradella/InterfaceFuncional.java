26d55bc3356a6da5968d6a3fbc4c537e831a7db7
Lucas <lucas.pradella@hotmail.com>
2017-07-31 14:41:12 -0300

    lambdas 2 interfaceFuncinal.

--- estudos-stream-lambdas-funcional/src/main/java/br/com/pradella/InterfaceFuncional.java
+++ estudos-stream-lambdas-funcional/src/main/java/br/com/pradella/InterfaceFuncional.java
@@ -2,13 +2,15 @@ package br.com.pradella;
 
 public class InterfaceFuncional {
 	
+	interface num{
+		double getValue();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("=====  Interfaces Funcionais =====");
-		
-		
-		
-		
-		
+
+		num n = () -> 10.00;
+		System.out.println(n.getValue());
 	}
 
 	
