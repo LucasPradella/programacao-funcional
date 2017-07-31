88f50dc6b069e3923b8aba528e8016c60626a634
Lucas <lucas.pradella@hotmail.com>
2017-07-31 15:52:33 -0300

    Utilizando  Interface Funcional Predicate.

--- estudos-stream-lambdas-funcional/src/main/java/br/com/pradella/stream/Stream.java
+++ estudos-stream-lambdas-funcional/src/main/java/br/com/pradella/stream/Stream.java
@@ -2,9 +2,21 @@ package br.com.pradella.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+
+
 
 public class Stream {
 
+	private static void filtro(List<String> lista, Predicate<String> condicao) {
+		for (String  s : lista) 
+			if (condicao.test(s)) 
+				System.out.println(s);
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 		
@@ -23,6 +35,31 @@ public class Stream {
 		//metodo reference
 		System.out.println("---------------");
 		lista1.forEach(System.out::println);
+		
+		
+		System.out.println("======PREDICATE======");
+		
+		
+		System.out.println("Estados que inicia com a letra S");
+		filtro(lista1, (s) -> s.startsWith("S") );
+		
+		System.out.println("------------------------------");
+		System.out.println("Estados que termina com a letra a");
+		filtro(lista1, (s) -> s.endsWith("a") );
+		
+		
+		System.out.println("------------------------------");
+		System.out.println("Imprimir toda a lista");
+		filtro(lista1, (s) -> true);
+		
+		System.out.println("------------------------------");
+		System.out.println("Nao imprimir toda a lista");
+		filtro(lista1, (s) -> false);
+		
+		
+		System.out.println("------------------------------");
+		System.out.println("Imprimir nomes com mais de 10 caracteres");
+		filtro(lista1, (s) -> s.length()>10);
 		
 		
 	}
